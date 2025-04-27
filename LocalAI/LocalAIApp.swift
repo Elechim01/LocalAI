00ea5a2e9146e75a450116d9f614833e2764df21
michelemanniello <michele.manniello@consultingmine.com>
2025-04-27 16:23:27 +0200

    Initial Commit

---
+++ LocalAI/LocalAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAIApp.swift
+//  LocalAI
+//
+//  Created by Michele Manniello on 27/04/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
